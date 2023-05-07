27d5ee54d753dd6018813ec79d7dd34167a752d4
Praneeth <vhpraneeth2001@gmail.com>
2023-05-07 20:37:25 +0530

    Updated code

--- block-goals-client/src/components/constants.jsx
+++ block-goals-client/src/components/constants.jsx
@@ -2,7 +2,7 @@
 // export const urlBase = 'https://cdn.jsdelivr.net/gh/vh-praneeth/web3_projects@main/commons_app';
 // export const commonsFolder = "../../../commons/src";
 export const projectName = 'Block Goals';
-import Commons from "../../../commons/src";
+import Commons from "../../../common-react-components/src";
 
 // have only 1 function so it can be changed later in 1 place
 // micro-frontend or web components can be used in future
