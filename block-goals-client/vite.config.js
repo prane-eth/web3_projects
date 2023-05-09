6e1c4ce617f51ec7fe7134adf30a4d86e67e7651
Praneeth <vhpraneeth2001@gmail.com>
2023-05-09 22:08:44 +0530

    Updated code

--- block-goals-client/vite.config.js
+++ block-goals-client/vite.config.js
@@ -1,5 +1,6 @@
 import react from "@vitejs/plugin-react";
 import federation from "@originjs/vite-plugin-federation";
+import path from 'path';
 
 // https://vitejs.dev/config/
 /** @type {import('vite').UserConfig} */
@@ -19,5 +20,11 @@ export default {
 		target: "esnext",
 		minify: false,
 		cssCodeSplit: false,
+	},
+	resolve: {
+		alias: {
+			"my-commons": path.resolve(__dirname, "../common-react-components/src"),
+			// "my-commons": "https://cdn.jsdelivr.net/gh/vh-praneeth/web3_projects@main/common-react-components/src/index.jsx",
+		},
 	},
 };
